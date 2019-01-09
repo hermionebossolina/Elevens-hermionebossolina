6b9e9b75a9f573d0e6393b9199988852c1428dd8
Hermione Bossolina <hermionebossolina@gmail.com>
2019-01-09 10:01:17 -0800

    Activity 1 : Hermione Bossolina

--- Activity1 Starter Code/CardTester.java
+++ Activity1 Starter Code/CardTester.java
@@ -8,6 +8,20 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	    String suit = "six";
+	    String rank = "four";
+	    int pointValue = 5;
+	    Card c = new Card(rank, suit, pointValue);
+	    System.out.println(c);
+	    suit = "red";
+	    rank = "eight";
+	    pointValue = 7;
+	    Card d = new Card(suit, rank, pointValue);
+	    System.out.println(d);
+	    suit = "5";
+	    rank = "7";
+	    pointValue = 14;
+	    Card e = new Card(rank, suit, pointValue);
+	    System.out.println(e);
 	}
 }
